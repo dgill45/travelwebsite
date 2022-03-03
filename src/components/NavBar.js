faef16378b12ac76af74a69467b168cc168ac67d
donellgill <donell.gill@outlook.com>
2022-03-03 14:07:29 -0500

    added navbar styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,24 @@
 import React, {useState} from 'react';
+import './Navbar.css'
 import {Link} from 'react-router-dom'
 
 function NavBar(){
 
     const [click, setClick] = useState(false)
+    const [button, setButton] =  useState(true)
 
     const handleClick = () => setClick(!click)
-
     const closeMobileMenu =()=> setClick(false)
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        }else{
+            setButton(true)
+        }
+    }
+
+    window.addEventListener('resize', showButton)
 
     return (
 
@@ -43,6 +54,7 @@ function NavBar(){
                         </Link>
                     </li>
                 </ul>
+                {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
             </div>
         </nav> 
 
